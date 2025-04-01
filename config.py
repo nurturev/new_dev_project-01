d4363e72d8d8f905e101ff45c95722c82473af3b
akshara-shukla <akshara.shukla@nurturev.com>
2025-04-01 16:49:23 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION", "us-east-1")
